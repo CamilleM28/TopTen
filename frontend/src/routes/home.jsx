94ae06ffba425d3925632b845a89bbe1bca8f479
CamilleM28 <Camillem2812@gmail.com>
2023-09-05 18:31:08 +0200

    displayed lists & styled page

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,6 +1,6 @@
 import { redirect, useLoaderData } from "react-router-dom";
 import getUser from "../getUser";
-import { GoogleLogin } from "@react-oauth/google";
+import "../styles/home.css";
 
 export async function loader(id) {
   const user = await getUser(id);
@@ -13,10 +13,41 @@ export async function loader(id) {
 export default function Home() {
   const { user } = useLoaderData();
   console.log(user);
+  console.log(user.lists.music);
+
+  const mapper = (list) => {
+    return (
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item === null ? `${index + 1}: ` : `${index + 1}: ${item}`}
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <>
-      <h1>Home</h1>
+      <h1 class="heading">TopTens</h1>
+      <div id="lists">
+        <div class="category" id="music">
+          <h2 class="heading">Music</h2>
+          {mapper(user.lists.music)}
+        </div>
+        <div class="category">
+          <h2 class="heading">Movies</h2>
+          {mapper(user.lists.movies)}
+        </div>
+        <div class="category" id="tv">
+          <h2 class="heading">TV</h2>
+          {mapper(user.lists.tv)}
+        </div>
+        <div class="category">
+          <h2 class="heading">Books</h2>
+          {mapper(user.lists.books)}
+        </div>
+      </div>
     </>
   );
 }
